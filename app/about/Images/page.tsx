4429aad735f8aa41bab0bcbd7d4d371e41e3074c
k4trinderoya1 <your.email@example.com>
2024-11-07 20:47:16 +0200

    Servv fixing 2

--- app/about/Images/page.tsx
+++ app/about/Images/page.tsx
@@ -1,5 +1,5 @@
 import { list } from '@vercel/blob';
-import ServvClient from '@/app/servv/page';
+import ServvClient from '@/app/servv/ServvClient';
 
 export default async function ServvPageServer() {
   const response = await list();
