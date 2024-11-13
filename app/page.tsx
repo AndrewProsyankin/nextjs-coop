ed8bc0feebb8e89d0359f9c39be902ce5b34023a
k4trinderoya1 <your.email@example.com>
2024-11-13 16:25:15 +0200

    img4

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 const HomePage = () => {
@@ -42,7 +41,7 @@ const HomePage = () => {
                     <div className="flex items-center space-x-6 lg:space-x-8">
                       <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
                         <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
-                          <Image
+                          <img
                             alt=""
                             src="/images/woman.jpg"
                             className="h-full w-full object-cover object-center"
@@ -51,7 +50,7 @@ const HomePage = () => {
                           />
                         </div>
                         <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
-                          <Image
+                          <img
                             alt=""
                             src="/images/krasnyi_cardigan.jpg"
                             className="h-full w-full object-cover object-center"
