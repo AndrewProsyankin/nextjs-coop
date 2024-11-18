214dfb2a59cba650a5f367dd7c647fb1cdeefa8d
k4trinderoya1 <your.email@example.com>
2024-11-18 17:57:50 +0200

    Products.tsx page design

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,8 @@ const nextConfig = {
       'via.placeholder.com', 
       'example.com', 
       'tailwindui.com', 
-      'vercel-storage.com',  // Добавляем нужный домен
-      '9ecgtmz1qx9a2h51.public.blob.vercel-storage.com'  // Ваш домен
+      'vercel-storage.com',  
+      '9ecgtmz1qx9a2h51.public.blob.vercel-storage.com'  
     ],
   },
 };
