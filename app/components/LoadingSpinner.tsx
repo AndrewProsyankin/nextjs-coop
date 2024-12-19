0eb8c9e6498b4dbee591564dc945cccaacc7bb69
k4trinderoya1 <katekrut412@gmail.com>
2024-12-19 11:35:57 +0200

    debug

--- app/components/LoadingSpinner.tsx
+++ app/components/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react';
 import cx from "classnames";
-import styles from "@/app/LoafingSpinner.module.css"; 
+import styles from "@/app/LoadingSpinner.module.css"; 
 
 export type LoadingSpinnerProps = {
     isLoading: boolean;
